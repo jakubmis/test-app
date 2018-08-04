115cd3b5c9ad4bb41b50df5e9d01b1fbf40b0c0a
jakubmis <jakub.mis@schibsted.pl>
2018-08-04 12:38:23 +0200

    #6 - Reduce scope of internal methods

--- src/main/scala/io/anymind/app/calculator/MathExpressionParser.scala
+++ src/main/scala/io/anymind/app/calculator/MathExpressionParser.scala
@@ -4,27 +4,27 @@ import scala.util.parsing.combinator.JavaTokenParsers
 
 class MathExpressionParser extends JavaTokenParsers {
 
-  def number: Parser[Operation] = floatingPointNumber ^^ { number => Number(number.toDouble) }
+  def parseExpression(expression: String): Either[String, Operation] = parseAll(expressionParser, expression) match {
+    case Success(result, _) => Right(result)
+    case Failure(error, _) => Left(error)
+  }
 
-  def expressionOrNumber: Parser[Operation] = "(" ~> expressionParser <~ ")" | number
+  private def number: Parser[Operation] = floatingPointNumber ^^ { number => Number(number.toDouble) }
 
-  def expressionParser: Parser[Operation] = multiplyOrDivide ~ rep("+" ~ multiplyOrDivide | "-" ~ multiplyOrDivide) ^^ {
+  private def expressionOrNumber: Parser[Operation] = "(" ~> expressionParser <~ ")" | number
+
+  private def expressionParser: Parser[Operation] = multiplyOrDivide ~ rep("+" ~ multiplyOrDivide | "-" ~ multiplyOrDivide) ^^ {
     case number ~ rest => rest.foldLeft(number) {
       case (x, "+" ~ y) => Add(x, y);
       case (x, "-" ~ y) => Subtract(x, y);
     }
   }
 
-  def multiplyOrDivide: Parser[Operation] = expressionOrNumber ~ rep("*" ~ expressionOrNumber | "/" ~ expressionOrNumber) ^^ {
+  private def multiplyOrDivide: Parser[Operation] = expressionOrNumber ~ rep("*" ~ expressionOrNumber | "/" ~ expressionOrNumber) ^^ {
     case number ~ rest => rest.foldLeft(number) {
       case (x, "*" ~ y) => Multiply(x, y);
       case (x, "/" ~ y) => Divide(x, y);
     }
-  }
-
-  def parseExpression(expression: String): Either[String, Operation] = parseAll(expressionParser, expression) match {
-    case Success(result, _) => Right(result)
-    case Failure(error, _) => Left(error)
   }
 
 }
